54c1177069c3743d5cb00000d07be8e156e81b95
wicked-ev <101883306+wicked-ev@users.noreply.github.com>
2025-11-21 17:53:34 +0100

    first commit

---
+++ src/main/java/com/example/school_backend_demo/SchoolBackendDemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.school_backend_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SchoolBackendDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolBackendDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/school_backend_demo/SchoolBackendDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.school_backend_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolBackendDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
